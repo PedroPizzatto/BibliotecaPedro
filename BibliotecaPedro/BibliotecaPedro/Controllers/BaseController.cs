1da83dbabf31e0791ece84c7b48286ff06eb038f
PedroPizzatto <pedro.pizzatto@gmail.com>
2017-11-02 15:14:32 -0200

    projeto mvc

---
+++ BibliotecaPedro/BibliotecaPedro/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BibliotecaPedro.Performance;
+using BibliotecaPedro.Models;
+
+namespace BibliotecaPedro.Controllers
+{
+    [CompressaoAtributos]
+    public class BaseController : Controller
+    {
+        protected BibliotecaEntities db;
+
+        public BaseController()
+        {
+           db = new BibliotecaEntities();
+        }
+    }
+}
---
+++ BibliotecaPedro/BibliotecaPedro/Performance/CompressaoAtributos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BibliotecaPedro.Performance
+{
+    public class CompressaoAtributos : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            var encondingsAccepted = filterContext.HttpContext.Request.Headers["Accept-Encoding"];
+                if (string.IsNullOrEmpty(encondingsAccepted)) return;
+
+            encondingsAccepted = encondingsAccepted.ToLowerInvariant();
+            
+            var response = filterContext.HttpContext.Response;
+
+            if(encondingsAccepted.Contains("deflate"))
+            {
+                response.AppendHeader("Content-encoding", "deflate");
+                response.Filter = new DeflateStream(response.Filter, CompressionMode.Compress);
+            }
+            else if(encondingsAccepted.Contains("gzip"))
+            {
+                response.AppendHeader("Content-encoding", "gzip");
+                response.Filter = new GZipStream(response.Filter, CompressionMode.Compress);
+            }
+        }
+    }
+}
